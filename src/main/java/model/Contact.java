82bcdad2d9cef0717178f5471e804bebf1f19402
martin.skuthan <martin.skuthan95@gmail.com>
2021-02-22 23:01:58 +0100

    Model - Add contact

---
+++ src/main/java/model/Contact.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.Objects;
+
+public class Contact {
+    private String contactName;
+    private String phoneNumber;
+
+    public Contact(String contactName, String phoneNumber) {
+        this.contactName = contactName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(contactName, contact.contactName) &&
+                Objects.equals(phoneNumber, contact.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contactName, phoneNumber);
+    }
+}
